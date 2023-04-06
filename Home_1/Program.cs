dad337e6dbec904153f6bedef77434c4d3322f08
Serge Bogoduhov <zz9250347500@gmail.com>
2023-04-06 15:43:41 +0300

    первой задачи

---
+++ Home_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+//  является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Vvedite pyateznachnoe cheslo: ");
+int numA = Convert.ToInt32(Console.ReadLiane());
+
+int i = 0;
+for (; i <= numA; i = numA / 10 % 10)
+{
+    i++;
+}
+if  (i(5) == i(1) || i(4) == i(2))
+{
+    Console.WriteLine("палиндром");
+}        
+Console.WriteLine("не палиндром");
